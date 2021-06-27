27ad77949112c0fa02409ec8993c228bed13a912
1terry <t.juottawa@gmail.com>
2021-06-27 04:35:18 -0400

    added video player

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ def index():
     return render_template('index.html')
 
 
-def gen(camera):
+def gen(main):
     while True:
-        frame = camera.get_frame()
+        frame = main.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(VideoCamera()),
+    return Response(gen(main()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
--- main.py
+++ main.py
@@ -5,84 +5,101 @@ from Finger import Finger
 from interpretation import interpret
 
 
-def main():
+class main(object):
 
-    # Finger objects
-    THUMB = Finger()
-    INDEX = Finger()
-    MIDDLE = Finger()
-    RING = Finger()
-    PINKY = Finger()
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
 
-    # captures video from webcam
-    # NOTE: input value can vary between -1, 0, 1, 2 (differs per device, 0 or 1 is common)
-    # WARNING: VideoCapture does not work if another application is using camera (ie. video calling)
-    cap = cv2.VideoCapture(0)
+    def get_frame(self):
+        success, image = self.video.read()
+        # image = cv2.resize(image, None, fx=ds_factor,
+        #                    fy=ds_factor, interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # face_rects = face_cascade.detectMultiScale(gray, 1.3, 5)
+        # for (x, y, w, h) in face_rects:
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #     break
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
 
-    # from pre-trained Mediapipe to draw hand landmarks and connections
-    mpHands = mp.solutions.hands
-    hands = mpHands.Hands(max_num_hands=1)
-    mpDraw = mp.solutions.drawing_utils
+    def getFrame(self):
+        # Finger objects
+        THUMB = Finger()
+        INDEX = Finger()
+        MIDDLE = Finger()
+        RING = Finger()
+        PINKY = Finger()
 
-    # used to calculate FPS
-    pTime = 0  # previous time
-    cTime = 0  # current time
+        # captures video from webcam
+        # NOTE: input value can vary between -1, 0, 1, 2 (differs per device, 0 or 1 is common)
+        # WARNING: VideoCapture does not work if another application is using camera (ie. video calling)
+        cap = cv2.VideoCapture(0)
 
-    while True:
-        # reads image from webcam
-        success, img = cap.read()
+        # from pre-trained Mediapipe to draw hand landmarks and connections
+        mpHands = mp.solutions.hands
+        hands = mpHands.Hands(max_num_hands=1)
+        mpDraw = mp.solutions.drawing_utils
 
-        # converts default image value to RGB value
-        # NOTE: when printing back to the screen, use default value (img) NOT imgRGB
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        imgRGB.flags.writeable = False  # improves performance
-        # use Mediapipe to process converted RGB value
-        results = hands.process(imgRGB)
+        # used to calculate FPS
+        pTime = 0  # previous time
+        cTime = 0  # current time
 
-        if results.multi_hand_landmarks:
+        while True:
+            # reads image from webcam
+            success, img = cap.read()
 
-            for handLms in results.multi_hand_landmarks:
-                # creates list of all landmarks for easier indexing
-                # list will have 21 values -> lm_list[0] will be first landmark
-                lm_list = []
+            # converts default image value to RGB value
+            # NOTE: when printing back to the screen, use default value (img) NOT imgRGB
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            imgRGB.flags.writeable = False  # improves performance
+            # use Mediapipe to process converted RGB value
+            results = hands.process(imgRGB)
 
-                # id corresponds to landmark #
-                #   -> 21 landmarks in total (4 on non-thumb fingers, rest on thumb and palm)
-                # lm corresponds to landmark value
-                #   -> each lm has x coordinate and y coordinate
-                #   -> default values are in ratio (value between 0 and 1)
-                #   -> to convert to pixel value, multiple by width and height of screen
-                for id, lm in enumerate(handLms.landmark):
-                    h, w, c = img.shape                 # get height, width, depth
-                    # convert to x, y pixel values
-                    cx, cy, cz = int(lm.x*w), int(lm.y*h), lm.z*c
+            if results.multi_hand_landmarks:
 
-                    lm_list.append([id, cx, cy, cz])
+                for handLms in results.multi_hand_landmarks:
+                    # creates list of all landmarks for easier indexing
+                    # list will have 21 values -> lm_list[0] will be first landmark
+                    lm_list = []
 
-                # writes text to screen
-                cv2.putText(img, str(interpret(lm_list)), (550, 70),
-                            cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
+                    # id corresponds to landmark #
+                    #   -> 21 landmarks in total (4 on non-thumb fingers, rest on thumb and palm)
+                    # lm corresponds to landmark value
+                    #   -> each lm has x coordinate and y coordinate
+                    #   -> default values are in ratio (value between 0 and 1)
+                    #   -> to convert to pixel value, multiple by width and height of screen
+                    for id, lm in enumerate(handLms.landmark):
+                        h, w, c = img.shape                 # get height, width, depth
+                        # convert to x, y pixel values
+                        cx, cy, cz = int(lm.x*w), int(lm.y*h), lm.z*c
 
-                # draw hand landmarks and connections
-                mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                        lm_list.append([id, cx, cy, cz])
 
-        # print FPS on screen (not console)
-        # cTime = time.time()
-        # fps = 1/(cTime-pTime)
-        # pTime = cTime
-        # cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3)
+                    # writes text to screen
+                    cv2.putText(img, str(interpret(lm_list)), (550, 70),
+                                cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 3)
 
-        # print current image captured from webcam
-        cv2.imshow("Image", img)
-        key = cv2.waitKey(1)
+                    # draw hand landmarks and connections
+                    mpDraw.draw_landmarks(
+                        img, handLms, mpHands.HAND_CONNECTIONS)
 
-        # press Q to quit or "stop" button
-        if key == ord("q"):
-            break
+            # print FPS on screen (not console)
+            # cTime = time.time()
+            # fps = 1/(cTime-pTime)
+            # pTime = cTime
+            # cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,0,255), 3)
 
-    # cleanup
-    cap.release()
-    cv2.destroyAllWindows()
+            # print current image captured from webcam
+            cv2.imshow("Image", img)
+            key = cv2.waitKey(1)
+
+            # press Q to quit or "stop" button
+            if key == ord("q"):
+                break
+
+        # cleanup
+        cap.release()
+        cv2.destroyAllWindows()
 
 
-main()
+# main()
